89e72b35176e6c04c8c231a55b11e0a8f2625f0c
darmawanfatriananda <darmawan.fatriananda@mandrill.com.my>
2025-09-09 22:06:21 +0700

    hide menu only for admin

--- web/src/layout/AppMenu.vue
+++ web/src/layout/AppMenu.vue
@@ -8,7 +8,7 @@ const authStore = useAuthStore();
 
 // Check if user is DEALER_USER role
 const isDealerUser = computed(() => {
-    return authStore.userRole === 'DEALER_USER';
+    return authStore.userRole === 'DEALER_USER' || authStore.userRole === 'DEALER_ADMIN'  ;
 });
 
 const model = ref([
@@ -17,7 +17,7 @@ const model = ref([
         items: [
             // Hide Dashboard items for DEALER_USER role
             ...(!isDealerUser.value ? [
-                { label: 'H1', icon: 'pi pi-fw pi-chart-bar', to: '/h1-dashboard' },
+                { label: 'H1 (Work In Progress)', icon: 'pi pi-fw pi-chart-bar', to: '/h1-dashboard' },
                 { label: 'H23', icon: 'pi pi-fw pi-chart-line', to: '/h23-dashboard' }
             ] : [])
         ]
